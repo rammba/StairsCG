99f08c2a3ded901d7d169a088a6eb7bfc4e46fa8
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2021-01-12 23:36:48 +0100

    1st control point done

---
+++ AssimpSample/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using SharpGL.SceneGraph;
+using SharpGL;
+using Microsoft.Win32;
+
+
+namespace AssimpSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Atributi
+
+        /// <summary>
+        ///	 Instanca OpenGL "sveta" - klase koja je zaduzena za iscrtavanje koriscenjem OpenGL-a.
+        /// </summary>
+        World m_world = null;
+
+        #endregion Atributi
+
+        #region Konstruktori
+
+        public MainWindow()
+        {
+            // Inicijalizacija komponenti
+            InitializeComponent();
+
+            // Kreiranje OpenGL sveta
+            try
+            {
+                m_world = new World(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "3D Models\\Bodymesh"), "Bodymesh.3DS", (int)openGLControl.ActualWidth, (int)openGLControl.ActualHeight, openGLControl.OpenGL);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Neuspesno kreirana instanca OpenGL sveta. Poruka greške: " + e.Message, "Poruka", MessageBoxButton.OK);
+                this.Close();
+            }
+        }
+
+        #endregion Konstruktori
+
+        /// <summary>
+        /// Handles the OpenGLDraw event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLDraw(object sender, OpenGLEventArgs args)
+        {
+            m_world.Draw(args.OpenGL);
+        }
+
+        /// <summary>
+        /// Handles the OpenGLInitialized event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLInitialized(object sender, OpenGLEventArgs args)
+        {
+            m_world.Initialize(args.OpenGL);
+        }
+
+        /// <summary>
+        /// Handles the Resized event of the openGLControl1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="args">The <see cref="SharpGL.SceneGraph.OpenGLEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_Resized(object sender, OpenGLEventArgs args)
+        {
+            m_world.Resize(args.OpenGL, (int)openGLControl.ActualWidth, (int)openGLControl.ActualHeight);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.F10: this.Close(); break;
+                case Key.W: m_world.RotationX -= 5.0f; break;
+                case Key.S: m_world.RotationX += 5.0f; break;
+                case Key.A: m_world.RotationY -= 5.0f; break;
+                case Key.D: m_world.RotationY += 5.0f; break;
+                case Key.Add: m_world.SceneDistance -= 3.0f; break;
+                case Key.Subtract: m_world.SceneDistance += 3.0f; break;
+                case Key.F2:
+                    OpenFileDialog opfModel = new OpenFileDialog();
+                    bool result = (bool) opfModel.ShowDialog();
+                    if (result)
+                    {
+
+                        try
+                        {
+                            World newWorld = new World(Directory.GetParent(opfModel.FileName).ToString(), Path.GetFileName(opfModel.FileName), (int)openGLControl.Width, (int)openGLControl.Height, openGLControl.OpenGL);
+                            m_world.Dispose();
+                            m_world = newWorld;
+                            m_world.Initialize(openGLControl.OpenGL);
+                        }
+                        catch (Exception exp)
+                        {
+                            MessageBox.Show("Neuspesno kreirana instanca OpenGL sveta:\n" + exp.Message, "GRESKA", MessageBoxButton.OK );
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ AssimpSample/World.cs
@@ -0,0 +1,294 @@
+﻿// -----------------------------------------------------------------------
+// <file>World.cs</file>
+// <copyright>Grupa za Grafiku, Interakciju i Multimediju 2013.</copyright>
+// <author>Srđan Mihić</author>
+// <author>Aleksandar Josić</author>
+// <summary>Klasa koja enkapsulira OpenGL programski kod.</summary>
+// -----------------------------------------------------------------------
+using System;
+using Assimp;
+using System.IO;
+using System.Reflection;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Primitives;
+using SharpGL.SceneGraph.Quadrics;
+using SharpGL.SceneGraph.Core;
+using SharpGL;
+
+namespace AssimpSample
+{
+
+
+    /// <summary>
+    ///  Klasa enkapsulira OpenGL kod i omogucava njegovo iscrtavanje i azuriranje.
+    /// </summary>
+    public class World : IDisposable
+    {
+        #region Atributi
+
+        /// <summary>
+        ///	 Scena koja se prikazuje.
+        /// </summary>
+        private AssimpScene m_scene;
+
+        /// <summary>
+        ///	 Ugao rotacije sveta oko X ose.
+        /// </summary>
+        private float m_xRotation = 0.0f;
+
+        /// <summary>
+        ///	 Ugao rotacije sveta oko Y ose.
+        /// </summary>
+        private float m_yRotation = 0.0f;
+
+        /// <summary>
+        ///	 Udaljenost scene od kamere.
+        /// </summary>
+        private float m_sceneDistance = 30f;
+
+        /// <summary>
+        ///	 Sirina OpenGL kontrole u pikselima.
+        /// </summary>
+        private int m_width;
+
+        /// <summary>
+        ///	 Visina OpenGL kontrole u pikselima.
+        /// </summary>
+        private int m_height;
+
+        #endregion Atributi
+
+        #region Properties
+
+        /// <summary>
+        ///	 Scena koja se prikazuje.
+        /// </summary>
+        public AssimpScene Scene
+        {
+            get { return m_scene; }
+            set { m_scene = value; }
+        }
+
+        /// <summary>
+        ///	 Ugao rotacije sveta oko X ose.
+        /// </summary>
+        public float RotationX
+        {
+            get { return m_xRotation; }
+            set { m_xRotation = value; }
+        }
+
+        /// <summary>
+        ///	 Ugao rotacije sveta oko Y ose.
+        /// </summary>
+        public float RotationY
+        {
+            get { return m_yRotation; }
+            set { m_yRotation = value; }
+        }
+
+        /// <summary>
+        ///	 Udaljenost scene od kamere.
+        /// </summary>
+        public float SceneDistance
+        {
+            get { return m_sceneDistance; }
+            set { m_sceneDistance = value; }
+        }
+
+        /// <summary>
+        ///	 Sirina OpenGL kontrole u pikselima.
+        /// </summary>
+        public int Width
+        {
+            get { return m_width; }
+            set { m_width = value; }
+        }
+
+        /// <summary>
+        ///	 Visina OpenGL kontrole u pikselima.
+        /// </summary>
+        public int Height
+        {
+            get { return m_height; }
+            set { m_height = value; }
+        }
+
+        #endregion Properties
+
+        #region Konstruktori
+
+        /// <summary>
+        ///  Konstruktor klase World.
+        /// </summary>
+        public World(String scenePath, String sceneFileName, int width, int height, OpenGL gl)
+        {
+            this.m_scene = new AssimpScene(scenePath, sceneFileName, gl);
+            this.m_width = width;
+            this.m_height = height;
+        }
+
+        /// <summary>
+        ///  Destruktor klase World.
+        /// </summary>
+        ~World()
+        {
+            this.Dispose(false);
+        }
+
+        #endregion Konstruktori
+
+        #region Metode
+
+        /// <summary>
+        ///  Korisnicka inicijalizacija i podesavanje OpenGL parametara.
+        /// </summary>
+        public void Initialize(OpenGL gl)
+        {
+            gl.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            gl.Color(1f, 0f, 0f);
+            // Model sencenja na flat (konstantno)
+            gl.ShadeModel(OpenGL.GL_FLAT);
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+            gl.Enable(OpenGL.GL_CULL_FACE);
+            gl.FrontFace(OpenGL.GL_CCW);
+            m_scene.LoadScene();
+            m_scene.Initialize();
+        }
+
+        /// <summary>
+        ///  Iscrtavanje OpenGL kontrole.
+        /// </summary>
+        public void Draw(OpenGL gl)
+        {
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.Viewport(0, 0, m_width, m_height);
+            gl.MatrixMode(OpenGL.GL_PROJECTION);      // selektuj Projection Matrix
+            gl.LoadIdentity();
+            gl.Perspective(50.0, (double)m_width / (double)m_height, 0.5, 20000.0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();
+
+            gl.PushMatrix();
+            gl.Translate(0.0f, 0.0f, -m_sceneDistance);
+            gl.Rotate(m_xRotation, 1.0f, 0.0f, 0.0f);
+            gl.Rotate(m_yRotation, 0.0f, 1.0f, 0.0f);
+
+            DrawHuman(gl);
+            DrawCylinder(gl);
+            DrawSurface(gl);
+            DrawStairs(gl);
+
+            gl.PopMatrix();
+            DrawText(gl);
+
+            gl.Flush();
+        }
+
+        private void DrawHuman(OpenGL gl)
+        {
+            gl.PushMatrix();
+            gl.Translate(5f, 0.7f, 0f);
+            m_scene.Draw();
+            gl.PopMatrix();
+        }
+
+        private void DrawText(OpenGL gl)
+        {
+            gl.Viewport(m_width / 2, 0, m_width / 2, m_height / 2);
+            gl.PushMatrix();
+            gl.DrawText(m_width - 350, 100, 1.0f, 0.0f, 0.0f, "Verdana italic", 10, "Predmet: Racunarska grafika");
+            gl.DrawText(m_width - 350, 80, 1.0f, 0.0f, 0.0f, "Verdana italic", 10, "Sk.god: 2020/21");
+            gl.DrawText(m_width - 350, 60, 1.0f, 0.0f, 0.0f, "Verdana italic", 10, "Ime: Rados");
+            gl.DrawText(m_width - 350, 40, 1.0f, 0.0f, 0.0f, "Verdana italic", 10, "Prezime: Milicev");
+            gl.DrawText(m_width - 350, 20, 1.0f, 0.0f, 0.0f, "Verdana italic", 10, "Sifra zad: 16.1");
+            gl.PopMatrix();
+            gl.Viewport(0, 0, m_width, m_height);
+        }
+
+        private void DrawCylinder(OpenGL gl)
+        {
+            gl.PushMatrix();
+            gl.Translate(0f, -1f, 0f);
+            gl.Rotate(-90f, 0f, 0f);
+            Cylinder cil = new Cylinder();
+            cil.Height = 10;
+            cil.BaseRadius = 2;
+            cil.TopRadius = 2;
+            cil.CreateInContext(gl);
+            cil.Render(gl, SharpGL.SceneGraph.Core.RenderMode.Render);
+            gl.PopMatrix();
+        }
+
+        private void DrawSurface(OpenGL gl)
+        {
+            gl.PushMatrix();
+            gl.Color(0f, 1f, 0f);
+
+            gl.Begin(OpenGL.GL_QUADS);
+            gl.Vertex(10f, -1f, -16.85f);
+            gl.Vertex(-10f, -1f, -16.85f);
+            gl.Vertex(-10f, -1f, 17f);
+            gl.Vertex(10f, -1f, 17f);
+            gl.End();
+            gl.PopMatrix();
+        }
+
+        private void DrawStairs(OpenGL gl)
+        {
+            Cube cube = new Cube();
+            for (int i = 0; i < 20; i++)
+            {
+                gl.PushMatrix();
+                gl.Color(0f, 0f, 1f);
+                gl.Rotate(0f, i*20f, 0f);
+                gl.Translate(2.9f/* - i * 0.5*/, -0.7f + i * 0.5, 0f/* - i * 0.4*/);
+                gl.Scale(0.8f, 0.2f, 0.2f);
+                cube.Render(gl, SharpGL.SceneGraph.Core.RenderMode.Render);
+                gl.PopMatrix();
+            }
+        }
+
+        /// <summary>
+        /// Podesava viewport i projekciju za OpenGL kontrolu.
+        /// </summary>
+        public void Resize(OpenGL gl, int width, int height)
+        {
+            m_width = width;
+            m_height = height;
+            gl.Viewport(0, 0, m_width, m_height);
+            gl.MatrixMode(OpenGL.GL_PROJECTION);      // selektuj Projection Matrix
+            gl.LoadIdentity();
+            gl.Perspective(50.0, (double)width / (double)height, 0.5, 20000.0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();                // resetuj ModelView Matrix
+        }
+
+        /// <summary>
+        ///  Implementacija IDisposable interfejsa.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                m_scene.Dispose();
+            }
+        }
+
+        #endregion Metode
+
+        #region IDisposable metode
+
+        /// <summary>
+        ///  Dispose metoda.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable metode
+    }
+}
